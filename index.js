e9d400ed0845f29885d53ad1214ca68a78ce4d0c
CraigSmith789 <craig789@gmail.com>
2021-02-12 18:54:47 -0500

    commit tree 06f1e605e2ac418ff41eea4483dce3a3dfd45cc0

--- index.js
+++ index.js
@@ -4,12 +4,21 @@ const fi = (function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
-    each: function() {
+    each: function(array, alert) {
+      let newArray = []
+      if (array  instanceof Array)
+       newArray = array.slice()
+      else
+       newArray = Object.values(array)
+      for(let i = 0; i < newArray.length; i++)
+        alert(newArray[i]);
+      return array
 
     },
 
-    map: function() {
+    map: function(array, alert) {
 
+      return array
     },
 
     reduce: function() {
